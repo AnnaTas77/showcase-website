2b459ed101c2ae210606dc13a63c2a402d70785f
Anna Tasheva <anna.tasheva90@gmail.com>
2023-10-12 12:30:56 +0100

    Added contect and styling to the website

---
+++ data/skills.js
@@ -0,0 +1,31 @@
+import JSIcon from "../assets/gradient-icons/icons8-javascript-800.png";
+import React from "../assets/gradient-icons/icons8-react-native-800.png";
+import NextJS from "../assets/icons/next-js-svgrepo-com.svg";
+import Firebase from "../assets/icons/icons8-google-firebase-console-800.png";
+import PostgreSQL from "../assets/icons/postgresql-svgrepo-com.svg";
+import Express from "../assets/gradient-icons/icons8-express-js-800.png";
+import NodeJS from "../assets/gradient-icons/icons8-node-js-800.png";
+import Jest from "../assets/icons/jest-svgrepo-com.svg";
+import CSS3Icon from "../assets/gradient-icons/icons8-css-800.png";
+import TailwindCSS from "../assets/gradient-icons/icons8-tailwind-css-800.png";
+import HTML5Icon from "../assets/gradient-icons/icons8-html-800.png";
+import Git from "../assets/icons/git-svgrepo-com.svg";
+import GitHub from "../assets/gradient-icons/icons8-github-800.png";
+import NPMIcon from "../assets/icons/npm-svgrepo-com.svg";
+
+export const skills = [
+    { label: "JavaScript", icon: JSIcon },
+    { label: "React", icon: React },
+    { label: "NextJS", icon: NextJS },
+    { label: "Firebase", icon: Firebase },
+    { label: "PostgreSQL", icon: PostgreSQL },
+    { label: "Express.js", icon: Express },
+    { label: "NodeJS", icon: NodeJS },
+    { label: "Jest", icon: Jest },
+    { label: "CSS3", icon: CSS3Icon },
+    { label: "Tailwind CSS", icon: TailwindCSS },
+    { label: "HTML5", icon: HTML5Icon },
+    { label: "Git", icon: Git },
+    { label: "GitHub", icon: GitHub },
+    { label: "NPM", icon: NPMIcon },
+];
